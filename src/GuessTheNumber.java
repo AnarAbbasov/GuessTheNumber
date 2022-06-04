eb307a920c67d6d1d6b5c92c67151628301b0703
Anar Abbasov <anar_abbasov@hotmail.com>
2022-06-04 08:16:38 -0700

    removed debugging lines

--- src/GuessTheNumber.java
+++ src/GuessTheNumber.java
@@ -15,14 +15,14 @@ public class GuessTheNumber {
         int guessed = rand.nextInt(20);
         Scanner myscanner = new Scanner(System.in);
         System.out.println("Hello what is your name");
-
+        boolean win=false;
         String userName = myscanner.nextLine();
         System.out.println("Well " + userName + " I am thinking of number between 1 and 20.");
         System.out.println("Well " + userName + " Take a quess.");
         String quess = myscanner.nextLine();
         int quess_counter=0;
         String game_continue="y";
-        System.out.println(guessed);
+        //System.out.println(guessed);
         while (quess_counter<6 && game_continue.compareTo("y")==0) {
             if (guessed == Integer.valueOf(quess)) {
                 System.out.println("Good job, " + userName + " You quessed my number in " + quess_counter + " guesses!");
@@ -33,17 +33,19 @@ public class GuessTheNumber {
                 System.out.println("Well " + userName + " I am thinking of number between 1 and 20.");
                 System.out.println("Well " + userName + " Take a quess.");
                 guessed = rand.nextInt(20);
-                System.out.println(guessed);
+                //System.out.println(guessed);
             }
 
-            if (guessed > Integer.valueOf(quess) ) {
+            if (guessed > Integer.valueOf(quess) && win==false) {
                 System.out.println("Your guess is too low");
                 quess_counter++;
+                win=false;
                 System.out.println("Take a guess");
 
             }
-            if (guessed < Integer.valueOf(quess) ) {
+            if (guessed < Integer.valueOf(quess)&& win==false ) {
                 quess_counter++;
+                win=false;
                 System.out.println("Your guess is too high");
                 System.out.println("Take a guess");
             }
