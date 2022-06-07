5cf0e6e46ad4ce50da5523c5128820482d6e0f60
Anar Abbasov <anar_abbasov@hotmail.com>
2022-06-07 07:28:30 -0700

    Added try catch exceptions

--- src/GuessTheNumber.java
+++ src/GuessTheNumber.java
@@ -24,33 +24,50 @@ public class GuessTheNumber {
         String game_continue="y";
         //System.out.println(guessed);
         while (quess_counter<6 && game_continue.compareTo("y")==0) {
-            if (guessed == Integer.valueOf(quess)) {
-                System.out.println("Good job, " + userName + " You quessed my number in " + quess_counter + " guesses!");
-                System.out.println("Would you like to play again?(y or n)");
-                game_continue = myscanner.nextLine();
-                if (game_continue.compareTo("n")==0){
-                return;}
-                System.out.println("Well " + userName + " I am thinking of number between 1 and 20.");
-                System.out.println("Well " + userName + " Take a quess.");
-                guessed = rand.nextInt(20);
-                //System.out.println(guessed);
+            try {
+                if (guessed == Integer.valueOf(quess)) {
+                    System.out.println("Good job, " + userName + " You quessed my number in " + quess_counter + " guesses!");
+                    System.out.println("Would you like to play again?(y or n)");
+                    game_continue = myscanner.nextLine();
+                    if (game_continue.compareTo("n") == 0) {
+                        return;
+                    }
+                    System.out.println("Well " + userName + " I am thinking of number between 1 and 20.");
+                    System.out.println("Well " + userName + " Take a quess.");
+                    guessed = rand.nextInt(20);
+                    //System.out.println(guessed);
+                }
+            } catch (Exception e) {
+                System.out.println("Parse Exception caught");
+                return;
+            }
+            try {
+                if (guessed > Integer.valueOf(quess) && win == false) {
+                    System.out.println("Your guess is too low");
+                    quess_counter++;
+                    win = false;
+                    System.out.println("Take a guess");
+
+                }
+            } catch (Exception e) {
+                System.out.println("Parse Exception caught");
+                return;
             }
 
-            if (guessed > Integer.valueOf(quess) && win==false) {
-                System.out.println("Your guess is too low");
-                quess_counter++;
-                win=false;
-                System.out.println("Take a guess");
 
-            }
-            if (guessed < Integer.valueOf(quess)&& win==false ) {
-                quess_counter++;
-                win=false;
-                System.out.println("Your guess is too high");
-                System.out.println("Take a guess");
-            }
-              quess = myscanner.nextLine();
+            try {
+                if (guessed < Integer.valueOf(quess) && win == false) {
+                    quess_counter++;
+                    win = false;
+                    System.out.println("Your guess is too high");
+                    System.out.println("Take a guess");
+                }
+                quess = myscanner.nextLine();
+            } catch (Exception e)
+        {
+            System.out.println("Parse Exception caught");
+            return;
         }
-
+        }
     }
 }
